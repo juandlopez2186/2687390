a3c6f9b3b7449fa684159519456674895aa4c7ae
S-117 <psyco2186@gmail.com>
2023-02-24 10:49:44 -0500

    proyecto con el reto no7 de la guia

--- proyecto2687390/src/Challenge10.java
+++ proyecto2687390/src/Challenge10.java
@@ -10,10 +10,24 @@ public class Challenge10 {
             System.out.println("digite el color de la balota");
             color=redeye.nextLine();
             switch(color){
+                case "rojo":
+                cost=(cost*10)/100;
+                System.out.println("felicidades a ganado un descuento de 10% en su compra dando como resultado "+cost);
+                break;
                 case "azul":
+                cost=(cost*30)/100;
+                System.out.println("felicidades a ganado un descuento de 30% en su compra dando como resultado "+cost);
+                break;
+                case "amarillo":
+                cost=(cost*50)/100;
+                System.out.println("felicidades a ganado un descuento de 50% en su compra dando como resultado "+cost);
+                break;
+                case "blanco":
+                System.out.println("felicidades a ganado una compra completamente gratis ");
                 break;
             }
 
         }
+    redeye.close();
     }
 }
